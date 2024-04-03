71c702951b6bfbd4c8685f517eba6c39411751c7
Rivus44 <rivus44@gmail.com>
2024-04-03 22:08:01 +0200

    Poprawiono klasy i bindy na przyciskach

--- main.py
+++ main.py
@@ -9,79 +9,78 @@ class Game:
     def __init__(self, nickname):
         self.nickname = nickname
 
+
 class Menu:
-    #def __init__(self):
-    #    self.nickname = nickname
+    def __init__(self):
+        self.root = tk.Tk()
+        self.menu_image = None
+
+    def end_fullscreen(self, event):
+        self.root.attributes('-fullscreen', False)
+
+    def start_fullscreen(self, event):
+        self.root.attributes('-fullscreen', True)
+
     def start_menu(self):
-        root = tk.Tk()
-        root.title("Milionerzy")
-        root.attributes('-fullscreen', True)
-        root.pack_propagate(False)
-        root.configure(background='#080E43')
+        self.root.title("Milionerzy")
+        self.root.attributes('-fullscreen', True)
+        self.root.pack_propagate(False)
+        self.root.configure(background='#080E43')
 
-        def end_fullscreen(event):
-            root.attributes('-fullscreen', False)
+        self.root.bind("<Escape>", self.end_fullscreen)
+        self.root.bind("<F11>", self.start_fullscreen)
 
-        def start_fullscreen(event):
-            root.attributes('-fullscreen', True)
+        canvas = tk.Canvas(self.root,
+                           width=self.root.winfo_screenwidth(),
+                           height=self.root.winfo_screenheight(),
+                           highlightthickness=0)
+        canvas.pack(fill=tk.BOTH,
+                    expand=True)
+
+        image = Image.open("photos/milionerzy2.png")
+        resized_image = image.resize((self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
+        self.menu_image = ImageTk.PhotoImage(resized_image)
+
+        canvas.create_image(0, 0,
+                            anchor=tk.NW,
+                            image=self.menu_image)
+
+        start_button_width = 300
+        start_button_height = 80
+        start_button_edge = 20
+
+        x1 = canvas.winfo_screenwidth() / 2 - start_button_width / 2
+        x2 = canvas.winfo_screenwidth() / 2 + start_button_width / 2
+        x3 = x1 - start_button_edge
+        x4 = x2 + start_button_edge
+        y1 = canvas.winfo_screenheight() / 2 - start_button_height / 2
+        y2 = canvas.winfo_screenheight() / 2 + start_button_height / 2
+        y3 = y4 = canvas.winfo_screenheight() / 2
+
+        start_button = canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                             outline='#4D5CDC',
+                                             fill='#080E43',
+                                             width=4)
+        start_button_text = canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                           text="START",
+                           font='Helvetica 26 bold',
+                           fill="white")
+
+        canvas.tag_bind(start_button, "<ButtonRelease-1>", self.start_game)
+        canvas.tag_bind(start_button_text, "<ButtonRelease-1>", self.start_game)
+
+    def mainloop(self):
+        self.root.mainloop()
+
+    def start_game(self, event):
+        print("Game started")
 
 
 def main():
-    # Create the main window
-    root = tk.Tk()
-    root.title("Milionerzy")
-    root.attributes('-fullscreen', True)
-    root.pack_propagate(False)
-    root.configure(background='#080E43')
+    menu = Menu()
+    menu.start_menu()
+    menu.mainloop()
 
-    def start_game():
-        print("Game started")
-
-    def end_fullscreen(event):
-        root.attributes('-fullscreen', False)
-
-    def start_fullscreen(event):
-        root.attributes('-fullscreen', True)
-
-    # Bind the Escape key to exit fullscreen
-    root.bind("<Escape>", end_fullscreen)
-    root.bind("<F11>", start_fullscreen)
-
-    # Create a canvas
-    canvas = tk.Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), highlightthickness=0)
-    canvas.pack(fill=tk.BOTH, expand=True)
-    print(root.winfo_screenwidth())
-    print(root.winfo_screenheight())
-
-    # image = tk.PhotoImage(file="C:/Users/rivus/PycharmProjects/Milionerzy/photos/milionerzy_studio.jpg")
-    image = ImageTk.PhotoImage(
-        Image.open("photos/milionerzy2.png").resize((root.winfo_screenwidth(), root.winfo_screenheight())))
-    # Display the image
-
-    # Display the image on the canvas
-    canvas.create_image(0, 0, anchor=tk.NW, image=image)
-
-    # Draw a polygon button
-    button_width = 300
-    button_height = 80
-    button_edge = 20
-    x1 = canvas.winfo_screenwidth() / 2 - button_width / 2
-    x2 = canvas.winfo_screenwidth() / 2 + button_width / 2
-    x3 = x1 - button_edge
-    x4 = x2 + button_edge
-    y1 = canvas.winfo_screenheight() / 2 - button_height / 2
-    y2 = canvas.winfo_screenheight() / 2 + button_height / 2
-    y3 = y4 = canvas.winfo_screenheight() / 2
-
-    button = canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2], outline='#4D5CDC', fill='#080E43',
-                                   width=4)
-    button_text = canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text="START", font='Helvetica 26 bold', fill="white")
-
-    canvas.tag_bind(button, "<Button-1>", start_game)
-
-    # Run the main loop
-    root.mainloop()
 
 if __name__ == '__main__':
     main()
-
