16b32b2bd516bbc714d31cd848b69c42af98903e
Rivus44 <rivus44@gmail.com>
2024-04-10 00:13:59 +0200

    Zmiany wizualne w interfejsie - dodane pole z kwotami do wygrania oraz mnóstwo logiki

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import numpy as np
 import openpyxl
 import random
 
+
 class Game:
-    def __init__(self, nickname):
+    def __init__(self, nickname, menu_frame, root):
         self.nickname = nickname
-        self.question_prize_list = [100, 200, 300, 500, 1000, 2000, 5000, 10000, 20000,
+        self.root = root
+        self.menu_frame = menu_frame
+        self.game_frame = None
+        self.background_image = None
+        self.canvas = None
+        self.prize_button_list = []
+        self.prize_button_text_list = []
+        self.question_prize_list = [0, 100, 200, 300, 500, 1000, 2000, 5000, 10000, 20000,
                                     40000, 75000, 125000, 250000, 500000, 1000000]
-        self.guaranteed_list = [1000, 40000]
+        self.guaranteed_list = [1000, 40000, 1000000]
         self.current_question = 0
         self.current_money = 0
         self.guaranteed = 0
@@ -18,12 +26,278 @@ class Game:
         self.questions_path = 'data/questions.xlsx'
         self.already_asked_path = 'data/already_asked.xlsx'
 
-    def switch_to_game_interface(self):
-        #TODO - Frames
-        return 0
+        # DO TESTOWANIA
+        self.question_label = None
+        self.switch_button = None
+
+    def start(self):
+        self.menu_frame.pack_forget()
+        # Create a frame for asking questions
+
+        self.canvas = tk.Canvas(self.root,
+                                width=self.root.winfo_screenwidth(),
+                                height=self.root.winfo_screenheight(),
+                                highlightthickness=0)
+        self.canvas.pack(fill=tk.BOTH,
+                         expand=True)
+
+        image = Image.open("photos/milionerzy_studio3.png")
+        resized_image = image.resize((self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
+        self.background_image = ImageTk.PhotoImage(resized_image)
+
+        self.canvas.create_image(0, 0,
+                                 anchor=tk.NW,
+                                 image=self.background_image)
+
+        # Here you can add widgets for asking questions
+        # For example:
+        self.question_label = tk.Label(self.canvas, text="What is the capital of France?", font="Helvetica 18",
+                                       fg="white", bg="#080E43")
+        self.question_label.pack()
+
+        # Create a button to switch back to the menu
+        self.switch_button = tk.Button(self.canvas, text="Switch to Menu", command=self.next_question)
+        self.switch_button.pack()
+
+        self.create_prize_buttons()
+
+        self.game_frame = tk.Frame(self.canvas, background="#080E43")
+        self.game_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def end_game(self):
+        print("Game has ended")
+        self.switch_to_menu()
+
+    def switch_to_menu(self):
+        self.canvas.destroy()
+        self.game_frame.pack_forget()
+        self.game_frame.destroy()
+        self.menu_frame.pack()
+
+    def create_prize_buttons(self):
+        start_button_width = 180
+        start_button_height = 40
+        start_button_edge = 15
+        padding = 5
+
+        x1 = self.canvas.winfo_screenwidth() / 100 * 92 - start_button_width / 2
+        x2 = self.canvas.winfo_screenwidth() / 100 * 92 + start_button_width / 2
+        x3 = x1 - start_button_edge
+        x4 = x2 + start_button_edge
+        y1 = self.canvas.winfo_screenheight() * 19 / 20 - start_button_height / 2
+        y2 = self.canvas.winfo_screenheight() * 19 / 20 + start_button_height / 2
+        y3 = y4 = self.canvas.winfo_screenheight() * 19 / 20
+
+        prize_button_0 = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                   outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_0_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                         text="0",
+                                                         font='Helvetica 15 bold',
+                                                         fill="orange")
+        self.prize_button_list.append(prize_button_0)
+        self.prize_button_text_list.append(prize_button_0_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_100 = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                    outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_100_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                      text="100",
+                                                      font='Helvetica 15 bold',
+                                                      fill="orange")
+        self.prize_button_list.append(prize_button_100)
+        self.prize_button_text_list.append(prize_button_100_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_200 = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                    outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_200_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                      text="200",
+                                                      font='Helvetica 15 bold',
+                                                      fill="orange")
+        self.prize_button_list.append(prize_button_200)
+        self.prize_button_text_list.append(prize_button_200_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_300 = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                    outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_300_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                      text="300",
+                                                      font='Helvetica 15 bold',
+                                                      fill="orange")
+        self.prize_button_list.append(prize_button_300)
+        self.prize_button_text_list.append(prize_button_300_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_500 = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                    outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_500_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                      text="500",
+                                                      font='Helvetica 15 bold',
+                                                      fill="orange")
+        self.prize_button_list.append(prize_button_500)
+        self.prize_button_text_list.append(prize_button_500_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_1k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                    outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_1k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                      text="1000",
+                                                      font='Helvetica 15 bold',
+                                                      fill="white")
+        self.prize_button_list.append(prize_button_1k)
+        self.prize_button_text_list.append(prize_button_1k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_2k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                     outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_2k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                       text="2000",
+                                                       font='Helvetica 15 bold',
+                                                       fill="orange")
+        self.prize_button_list.append(prize_button_2k)
+        self.prize_button_text_list.append(prize_button_2k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_5k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                     outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_5k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                       text="5000",
+                                                       font='Helvetica 15 bold',
+                                                       fill="orange")
+        self.prize_button_list.append(prize_button_5k)
+        self.prize_button_text_list.append(prize_button_5k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_10k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                     outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_10k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                       text="10000",
+                                                       font='Helvetica 15 bold',
+                                                       fill="orange")
+        self.prize_button_list.append(prize_button_10k)
+        self.prize_button_text_list.append(prize_button_10k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_20k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_20k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="20000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="orange")
+        self.prize_button_list.append(prize_button_20k)
+        self.prize_button_text_list.append(prize_button_20k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_40k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_40k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="40000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="white")
+        self.prize_button_list.append(prize_button_40k)
+        self.prize_button_text_list.append(prize_button_40k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_75k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_75k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="75000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="orange")
+        self.prize_button_list.append(prize_button_75k)
+        self.prize_button_text_list.append(prize_button_75k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_125k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_125k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="125000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="orange")
+        self.prize_button_list.append(prize_button_125k)
+        self.prize_button_text_list.append(prize_button_125k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_250k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_250k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="250000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="orange")
+        self.prize_button_list.append(prize_button_250k)
+        self.prize_button_text_list.append(prize_button_250k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_500k = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_500k_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="500000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="orange")
+        self.prize_button_list.append(prize_button_500k)
+        self.prize_button_text_list.append(prize_button_500k_text)
+        y1 -= (40 + padding)
+        y2 -= (40 + padding)
+        y3 -= (40 + padding)
+        y4 -= (40 + padding)
+        prize_button_1m = self.canvas.create_polygon([x3, y3, x1, y1, x2, y1, x4, y4, x2, y2, x1, y2],
+                                                      outline='#4D5CDC', fill='#080E43', width=0)
+        prize_button_1m_text = self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2,
+                                                        text="1000000",
+                                                        font='Helvetica 15 bold',
+                                                        fill="white")
+        self.prize_button_list.append(prize_button_1m)
+        self.prize_button_text_list.append(prize_button_1m_text)
+
     def next_question(self):
-        #TODO - Frames
-        return 0
+        if self.current_question != 15:
+            self.current_question = self.current_question + 1
+            self.load_new_question()
+            self.current_money = self.question_prize_list[self.current_question]
+            if self.current_money in self.guaranteed_list:
+                self.guaranteed = self.current_money
+            self.update_prize_buttons()
+            print(f"Current question: {self.current_question}")
+            print(f"Current money: {self.current_money}")
+            print(f"Guaranteed: {self.guaranteed}")
+        else:
+            self.end_game()
+
+    def update_prize_buttons(self):
+        for i in range(len(self.prize_button_list)):
+            self.canvas.itemconfig(self.prize_button_list[i], outline='#080E43', fill='#4D5CDC')
+        self.canvas.itemconfig(self.prize_button_list[self.current_question], outline='#080E43', fill='yellow')
+
+    def load_new_question(self):
+        print("New question loaded")
 
     def pick_random_question(self, prize):
         result = [q for q in self.question_list if q.prize == prize]
@@ -66,10 +340,11 @@ class Question:
 class Menu:
     def __init__(self):
         self.root = tk.Tk()
-        self.menu_image = None
+        self.background_image = None
         self.start_button = None
         self.start_button_text = None
         self.canvas = None
+        self.menu_frame = None
 
     def end_fullscreen(self, event):
         self.root.attributes('-fullscreen', False)
@@ -102,11 +377,11 @@ class Menu:
 
         image = Image.open("photos/milionerzy2.png")
         resized_image = image.resize((self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
-        self.menu_image = ImageTk.PhotoImage(resized_image)
+        self.background_image = ImageTk.PhotoImage(resized_image)
 
         self.canvas.create_image(0, 0,
                                  anchor=tk.NW,
-                                 image=self.menu_image)
+                                 image=self.background_image)
 
         start_button_width = 300
         start_button_height = 80
@@ -136,14 +411,22 @@ class Menu:
         self.canvas.tag_bind(self.start_button, "<Leave>", self.on_stop_hover)
         self.canvas.tag_bind(self.start_button_text, "<Leave>", self.on_stop_hover)
 
+        self.menu_frame = tk.Frame(self.canvas, background="#080E43")
+        self.menu_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.menu_frame.lower()
+
     def mainloop(self):
         self.root.mainloop()
 
     def start_game(self, event):
         print("Game started")
-        game = Game("Test")
+        # Hide the menu frame
+        self.menu_frame.pack_forget()
+        game = Game("Test", self.canvas, self.root)
         game.read_questions()
-        game.next_question()
+        # game.next_question()
+        game.start()
+        game.update_prize_buttons()
 
     def on_hover(self, event):
         self.canvas.itemconfig(self.start_button, outline='#080E43', fill='#4D5CDC')
